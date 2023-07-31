e5a7cc264e6cc0da45f59ab73c6fd22b436fc76a
dsadsaddd1 <132988170+dsadsaddd1@users.noreply.github.com>
2023-07-31 09:45:55 +0900

    Delete kerly_v9 directory

--- kerly_v9/src/main/java/kr/co/tj/file/UrlRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.tj.file;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
-
-	UrlEntity save(UrlEntity entity);
-}
